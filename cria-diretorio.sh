e9960c3f21d4daa3dceda8077047b28e5e8bdefc
root <root@DESKTOP-6SF0CRD.>
2025-05-05 20:13:44 -0300

    Adiciona Script bash de criação de diretório e arquivo

---
+++ cria-diretorio.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+DIR="/home/lucas-pc/devops/exercicios"
+ARQUIVO="$DIR/bash"
+CONTEUDO="enviando conteudo para o arquivo"
+
+
+mkdir -p "$DIR" || { echo "Erro ao criar o diretório"; exit 1; }
+
+
+echo "$CONTEUDO" > "$ARQUIVO" || { echo "Erro ao escrever no arquivo"; exit 1; }
+
+echo "Arquivo '$ARQUIVO' criado com sucesso com o conteúdo:"
+cat "$ARQUIVO"
